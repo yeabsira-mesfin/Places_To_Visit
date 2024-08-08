d3aa388d786fee14adb5cd088f38580781e150d8
yeabsira-mesfin <yeabsira.mesfin29@gmail.com>
2024-08-08 06:59:12 -0400

    places in local storage.

--- src/components/AvailablePlaces.jsx
+++ src/components/AvailablePlaces.jsx
@@ -1,10 +1,17 @@
+import { useState } from 'react';
 import Places from './Places.jsx';
 
+const places = localStorage.getItem('places');
+
 export default function AvailablePlaces({ onSelectPlace }) {
+  const [availablePlaces,setAvailablePlaces] = useState([]);
+
+  
+
   return (
     <Places
       title="Available Places"
-      places={[]}
+      places={availablePlaces}
       fallbackText="No places available."
       onSelectPlace={onSelectPlace}
     />
