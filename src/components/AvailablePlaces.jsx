5e0a13438c17e3be97ababa36c0bd8a765901f90
yeabsira-mesfin <yeabsira.mesfin29@gmail.com>
2024-08-09 06:45:59 -0400

    Sorting places by location.

--- src/components/AvailablePlaces.jsx
+++ src/components/AvailablePlaces.jsx
@@ -1,33 +1,43 @@
-import { useState,useEffect } from 'react';
-import Error from './Error.jsx';
-import Places from './Places.jsx';
+import { useState, useEffect } from "react";
+import Error from "./Error.jsx";
+import Places from "./Places.jsx";
+import { sortPlacesByDistance } from "../loc.js";
 
-const places = localStorage.getItem('places');
+const places = localStorage.getItem("places");
 
 export default function AvailablePlaces({ onSelectPlace }) {
-  const [availablePlaces,setAvailablePlaces] = useState([]);
-  const [isLoading,setisLoading] = useState(true);
-  const [error,setError] = useState();
+  const [availablePlaces, setAvailablePlaces] = useState([]);
+  const [isLoading, setisLoading] = useState(true);
+  const [error, setError] = useState();
 
-  useEffect(()=>{
-
-    async function fetchPlaces(){
-      try{
-
-        const response = await  fetch('http://localhost:3000/places');
+  useEffect(() => {
+    async function fetchPlaces() {
+      try {
+        const response = await fetch("http://localhost:3000/places");
         const resData = await response.json();
-  
-        if(!response.ok){
-          throw new Error('Failed to fetch places');
-           
-        }
-        setAvailablePlaces(resData.places)
-      } catch(error){
-      setError({message:error.message || "Couldn't fetch places, please try again later!"})
-      }
-    
-      setisLoading(false)
 
+        if (!response.ok) {
+          throw new Error("Failed to fetch places");
+        }
+
+        navigator.geolocation.getCurrentPosition((position) => {
+          const sortedPlaces = sortPlacesByDistance(
+            resData.places,
+            position.coords.latitude,
+            position.coords.longitude
+          );
+          setAvailablePlaces(sortedPlaces);
+          setisLoading(false);
+        });
+      } catch (error) {
+        setError({
+          message:
+            error.message || "Couldn't fetch places, please try again later!",
+        });
+        setisLoading(false);
+      }
+
+    
     }
     fetchPlaces();
     // fetch('http://localhost:3000/places').then((response)=>{
@@ -35,19 +45,18 @@ export default function AvailablePlaces({ onSelectPlace }) {
     // }).then((resData)=>{
     //   setAvailablePlaces(resData.places);
     // });
-  },[]);
+  }, []);
 
-  if(error){
-    return <Error title="An error occurred!" message={error.message}/>
+  if (error) {
+    return <Error title="An error occurred!" message={error.message} />;
   }
-
 
   return (
     <Places
       title="Available Places"
       places={availablePlaces}
       isLoading={isLoading}
-      loadingText={'Fetching data...'}
+      loadingText={"Fetching data..."}
       fallbackText="No places available."
       onSelectPlace={onSelectPlace}
     />
