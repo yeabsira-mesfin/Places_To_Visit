b056d2c629d24992c3521adca5af8220d22a8b7f
yeabsira-mesfin <yeabsira.mesfin29@gmail.com>
2024-08-10 09:03:36 -0400

    User data sent to localstorate

--- src/http.js
+++ src/http.js
@@ -8,3 +8,17 @@ export async function fetchAvailablePlaces() {
 
   return resData.places;
 }
+export async function updateUserPlaces(places){
+const response = await fetch('http://localhost:3000/user-places',{
+  method:'PUT',// The method we need to use
+  body:JSON.stringify({places:places}), // Which data should be attached as a request body to the outgoing request.
+  headers: {
+    'Content-Type': 'application/json'
+  } 
+});
+
+const resData = await response.json();
+if(!response.ok){
+  throw new Error('Failed to update user data')
+}
+}
