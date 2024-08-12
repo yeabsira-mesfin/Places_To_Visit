7634f5357b5cd17a0b5e751db332dd20baa18064
yeabsira-mesfin <yeabsira.mesfin29@gmail.com>
2024-08-12 06:01:55 -0400

    Skeleton loading

--- src/components/Places.jsx
+++ src/components/Places.jsx
@@ -1,23 +1,42 @@
-export default function Places({ title, places, fallbackText, onSelectPlace,isLoading,loadingText }) {
+import image from '../assets/sl.png';
+
+export default function Places({ title, places = [], fallbackText, onSelectPlace, isLoading, loadingText }) {
   console.log(places);
+  
+  const loadingArray = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18];
+
   return (
     <section className="places-category">
       <h2>{title}</h2>
-      {isLoading && <p className="fallback-text">{loadingText}</p>}
-     { !isLoading && places.length === 0 && <p className="fallback-text">{fallbackText}</p>}
-      { !isLoading && places.length > 0 && (
+      
+      {isLoading ? (
         <ul className="places">
-          {places.map((place) => (
-            <li key={place.id} className="place-item">
-              <button onClick={() => onSelectPlace(place)}>
-                <img src={`http://localhost:3000/${place.image.src}`} alt={place.image.alt} />
-                <h3>{place.title}</h3>
+          {loadingArray.map((_, index) => (
+            <li key={index} className="place-item">
+              <button>
+                <img src={image} alt="Loading placeholder" />
+                <h3>{loadingText}</h3>
               </button>
             </li>
           ))}
         </ul>
+      ) : (
+        <>
+          {places.length === 0 && <p className="fallback-text">{fallbackText}</p>}
+          {places.length > 0 && (
+            <ul className="places">
+              {places.map((place) => (
+                <li key={place.id} className="place-item">
+                  <button onClick={() => onSelectPlace(place)}>
+                    <img src={`http://localhost:3000/${place.image.src}`} alt={place.image.alt} />
+                    <h3>{place.title}</h3>
+                  </button>
+                </li>
+              ))}
+            </ul>
+          )}
+        </>
       )}
     </section>
   );
 }
-// Don't forget to add skeleton when loading instead of fallback text.
